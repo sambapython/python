93295a490f6fd8d8ebaa6633cd6334807602dc84
Udhay Prakash Pethakamsetty <uday3prakash@gmail.com>
2017-09-02 18:46:31 +0530

    addressed a typo

--- call_api.py
+++ call_api.py
@@ -3,5 +3,5 @@ from pprint import pprint # pprint helps in better illustration of json structur
 resp = requests.get('http://host:port/users')
 pprint(resp.json())
 
-resp = request.post("http://host:port/users", json={key:value})
+resp = requests.post("http://host:port/users", json={key:value})
 pprint(resp.json())
